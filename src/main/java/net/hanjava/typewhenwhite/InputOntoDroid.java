257c000219aab3b823829772b55d8ee521cecf2e
Alan Goo <accent@thinkfree.com>
2013-06-14 14:53:24 +0900

    handle complete KeyEvent mapping

--- src/main/java/net/hanjava/typewhenwhite/InputOntoDroid.java
+++ src/main/java/net/hanjava/typewhenwhite/InputOntoDroid.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Alan Goo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.hanjava.typewhenwhite;
 
 import com.android.chimpchat.ChimpChat;
@@ -35,13 +51,7 @@ public class InputOntoDroid extends JLabel implements KeyListener, FocusListener
 
     @Override
     public void keyTyped(KeyEvent keyEvent) {
-        System.out.println("[InputOntoDroid] "+keyEvent);
-
-        try {
-            AwtDroidExchange.handleKeyTyped(keyEvent, device.getManager());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        // nothing to do
     }
 
     @Override
@@ -56,7 +66,12 @@ public class InputOntoDroid extends JLabel implements KeyListener, FocusListener
 
     @Override
     public void keyReleased(KeyEvent keyEvent) {
-        // do nothing
+        System.out.println("[InputOntoDroid] "+keyEvent);
+        try {
+            AwtDroidExchange.handleKeyReleased(keyEvent, device.getManager());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
